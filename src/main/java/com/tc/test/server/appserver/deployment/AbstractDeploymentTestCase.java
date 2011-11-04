0010d2451c6623cdbce2553aa4df76cce3a8f213
hung huynh <hhuynh-unknown@terracottatech.com>
2011-11-04 21:51:00 +0000

    new timebomb API
    svn path=/projects/container-test-framework/branches/tc-3.5/; revision=30291

--- src/main/java/com/tc/test/server/appserver/deployment/AbstractDeploymentTestCase.java
+++ src/main/java/com/tc/test/server/appserver/deployment/AbstractDeploymentTestCase.java
@@ -17,7 +17,6 @@ import com.tc.util.TcConfigBuilder;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -179,7 +178,7 @@ public abstract class AbstractDeploymentTestCase extends TCTestCase {
   }
 
   void disableAllTests() {
-    this.disableAllUntil(new Date(Long.MAX_VALUE));
+    disableTest();
   }
 
   private boolean shouldDisableForVariants() {
